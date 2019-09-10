461addd336884b0eb4db05ab34322ac67ac344ce
Eleasah Halsmer <eleasahjh@gmail.com>
2019-09-09 20:15:07 -0600

    Initial commit

---
+++ src/HiLo.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class HiLo {
+
+	public static void main(String[] args) {
+		// telling Scanner class to use keyboard input as its source
+		Scanner scan = new Scanner(System.in);
+		// play again, y or n
+		String playAgain = "";
+		// create a random number for user to guess
+		do {
+			int number = (int)(Math.random()*100 + 1);
+			//System.out.println(number);
+			int guess = 0;
+			// prompt the user:
+			while (guess != number) {
+				System.out.println("Guess a number between 1 and 100:");
+				guess = scan.nextInt();
+				System.out.println("You entered " + guess + ".");
+				if (guess < number) 
+					System.out.println(guess + " is too low, try again");
+				else if (guess > number)
+					System.out.println(guess + " is too high, try again");
+				else 
+					System.out.println(guess + " is correct. You win!");
+			}
+			System.out.println("Do you want to play again (y/n)?");
+			playAgain = scan.next();
+		} while (playAgain.equalsIgnoreCase("y"));
+		System.out.println("Thanks for playing! Goodbye");
+		scan.close();
+	}
+}
